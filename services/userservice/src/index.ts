8595e25b7c7a57408ef4bb3e8074285acdeebb32
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-05-30 13:45:40 +0500

    user model and try catch file created

--- services/userservice/src/index.ts
+++ services/userservice/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db";
+import userRoutes from "./routes/user.routes";
+
 dotenv.config();
 connectDb();
 
@@ -10,6 +12,8 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("User service is up and running!"); // Basic health check endpoint
 });
+
+app.use("api/v1/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`User service is running on port ${PORT}`);
