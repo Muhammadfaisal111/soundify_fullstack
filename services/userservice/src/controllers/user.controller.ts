52af8bdcb5c2295cbb7eb9daf2bbfea942c1aebc
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-05-30 15:51:46 +0500

    login controller and testing through postman

--- services/userservice/src/controllers/user.controller.ts
+++ services/userservice/src/controllers/user.controller.ts
@@ -38,3 +38,31 @@ export const registerUser = TryCatch(async (req, res) => {
     },
   });
 });
+
+export const loginUser = TryCatch(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: "Invalid email or password" });
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid email or password" });
+  }
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET!, {
+    expiresIn: "1d",
+  });
+  res.status(200).json({
+    message: "User logged in successfully",
+    token,
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+    },
+  });
+});
--- services/userservice/src/routes/user.routes.ts
+++ services/userservice/src/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import { registerUser } from "../controllers/user.controller";
+import { registerUser, loginUser } from "../controllers/user.controller";
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 export default router;
